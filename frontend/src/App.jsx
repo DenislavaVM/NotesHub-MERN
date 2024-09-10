54eb3556a1fe8351ce56ed6a3eec2139b2265e4f
Deni <denislavavmilanova@outlook.com>
2024-09-10 15:06:06 +0300

    Update App.jsx to include Router and Routes setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import Home from './pages/Home/Home'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import SignUp from './pages/SignUp/SignUp';
 
 const App = () => {
   return (
-    <div>
-      <Home />
-    </div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path='/dashboard' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
