1fdb75546f166d77c0e9344ab7736435c404276b
Deni <denislavavmilanova@outlook.com>
2024-09-11 17:33:54 +0300

    Add Navbar to Home component and integrate ProfileInfo for user display

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,21 @@
-import React from "react"
-import "./Navbar.css"
+import React from "react";
+import "./Navbar.css";
+import ProfileInfo from "../Cards/ProfileInfo";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+
+  const navigate = useNavigate;
+
+  const onLogout = () => {
+    navigate("/login")
+  }
+
   return (
     <div className="navbar">
       <h2 className="navbar-title">Notes</h2>
+
+      <ProfileInfo onLogout={onLogout}/>
     </div>
   );
 };
