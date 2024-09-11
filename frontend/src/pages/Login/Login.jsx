3cf8ddc2fb7c4b40a1e0158bbfd9dc218bd218d7
Deni <denislavavmilanova@outlook.com>
2024-09-11 13:39:09 +0300

    Update Login.jsx to include navigation and login form

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,9 +1,30 @@
-import React from "react"
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import { Link } from "react-router-dom";
+import "./Login.css";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <>
+      <Navbar />
+      <div className="login-container">
+        <div className="login-form">
+          <h4 className="login-title">Login</h4>
+          <form onSubmit={() => {}}>
+            <input type="text" placeholder="Email" className="input-box" />
+            <button type="submit" className="btn-primary">Login</button>
+          </form>
+          <p className="register-prompt">
+            Not registered yet?{" "}
+            <Link to="/signUp" className="signup-link">
+              Create an account
+            </Link>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Login
+
+export default Login;
