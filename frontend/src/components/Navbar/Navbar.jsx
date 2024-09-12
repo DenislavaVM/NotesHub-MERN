ca95c3599cbb53437bac6593495ce860c48a8e93
Deni <denislavavmilanova@outlook.com>
2024-09-12 11:27:41 +0300

    Add search functionality to Navbar component

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import ProfileInfo from "../Cards/ProfileInfo";
 import { useNavigate } from "react-router-dom";
 import SearchBar from "../searchBar/SearchBar";
 
 const Navbar = () => {
-
+  const [searchQuery, setSearchQuery] = useState("");
   const navigate = useNavigate;
 
   const onLogout = () => {
     navigate("/login")
   }
 
+  const handleSearch = () => {
+
+  };
+
+  const onClearSearch = () => {
+    setSearchQuery("");
+  };
+
   return (
     <div className="navbar">
       <h2 className="navbar-title">Notes</h2>
 
       <div className="navbar-center">
-        <SearchBar />
+        <SearchBar value={searchQuery}
+        onChange={({ target }) => {
+          setSearchQuery(target.value);
+        }}
+        handleSearch={handleSearch}
+        onClearSearch={onClearSearch}
+        />
       </div>
 
       <ProfileInfo onLogout={onLogout}/>
