288cf57a3af7842209aa15f71e3f544f012df56a
Deni <denislavavmilanova@outlook.com>
2024-09-13 16:33:42 +0300

    Add edit note functionality and improve backend APIs

--- backend/index.js
+++ backend/index.js
@@ -166,6 +166,57 @@ app.post("/add-note", authenticateToken, async (req, res) => {
     }
 });
 
+app.put("/edit-note/:noteId", authenticateToken, async (req, res) => {
+    const nodeId = req.params.noteId;
+    const { title, content, tags, isPinned } = req.body;
+    const { user } = req.user;
+
+    if (!title && !content && !tags) {
+        return res
+            .status(400)
+            .json({ error: false, message: "No changes provided" });
+    }
+
+    try {
+        const note = await Note.findOne({ _id: nodeId, userId: user._id });
+
+        if (!note) {
+            return res
+                .status(404)
+                .json({ error: true, message: "Note not found" });
+        }
+
+        if (title) {
+            note.title = title;
+        }
+
+        if (content) {
+            note.content = content;
+        }
+
+        if (tags) {
+            note.tags = tags;
+        }
+
+        if (isPinned) {
+            note.isPinned = isPinned;
+        }
+
+        await note.save();
+
+        return res.json({
+            error: false,
+            note,
+            message: "Note updated successfully",
+        });
+    } catch (error) {
+        return res.status(500).json({
+            error: true,
+            message: "Internal server error",
+        });
+    }
+});
+
 app.listen(3000, () => console.log("Server started on port 3000"));
 
 module.exports = app;
