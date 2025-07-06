54734ee7ce6dcbd03a7c26bd5a461016716f028d
Deni <denislavavmilanova@outlook.com>
2025-07-06 14:01:13 +0300

    refactor(routes): streamline label routes and add validation

--- backend/routes/label.routes.js
+++ backend/routes/label.routes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const labelController = require("../controllers/label.controller");
 const { authenticateToken } = require("../utils");
+const { validateLabel } = require("../middleware/validation");
 
 const router = express.Router();
 
-router.get("/labels", authenticateToken, labelController.getAllLabels);
-router.post("/labels", authenticateToken, labelController.createLabel);
-router.put("/labels/:labelId", authenticateToken, labelController.updateLabel);
-router.delete("/labels/:labelId", authenticateToken, labelController.deleteLabel);
+router.get("/", authenticateToken, labelController.getAllLabels);
+router.post("/", authenticateToken, validateLabel, labelController.createLabel);
+router.put("/:labelId", authenticateToken, validateLabel, labelController.updateLabel);
+router.delete("/:labelId", authenticateToken, labelController.deleteLabel);
 
 module.exports = router;
