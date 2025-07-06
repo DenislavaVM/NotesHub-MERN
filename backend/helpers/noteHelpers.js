edbfa0b6da9bbca64b11ddae408a661ed96c2b47
Deni <denislavavmilanova@outlook.com>
2025-07-06 13:55:01 +0300

    refactor(noteHelpers): rename populated field from 'tags' to 'labels' in getNoteByIdWithUser

--- backend/helpers/noteHelpers.js
+++ backend/helpers/noteHelpers.js
@@ -1,22 +1,7 @@
-const Note = require("../models/note.model");
-
-exports.findUserNote = async (noteId, userId) => {
-    return await Note.findOne({ _id: noteId, userId });
-};
-
-exports.findAndUpdateNote = async (noteId, userId, updateData) => {
-    const note = await Note.findOne({ _id: noteId, userId });
-    if (!note) {
-        throw new Error("Note not found");
-    };
-
-    Object.assign(note, updateData);
-    await note.save();
-    return note;
-};
+const Note = require("../models/note.model.js");
 
 exports.getNoteByIdWithUser = async (noteId) => {
     return await Note.findById(noteId)
         .populate("userId", "firstName lastName email")
-        .populate("tags", "name");
+        .populate("labels", "name");
 };
