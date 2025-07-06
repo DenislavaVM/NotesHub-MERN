476744f3683ef7443f18571c6f6395c90c42767b
Deni <denislavavmilanova@outlook.com>
2025-07-06 14:09:00 +0300

    chore: add routing and context providers in main entry point

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,34 @@
 import { StrictMode } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
+import { SocketProvider } from "./context/SocketContext.jsx";
+import { LabelsProvider } from "./context/LabelsProvider.jsx";
+import { NotificationProvider } from "./context/NotificationProvider.jsx";
+import { NotesProvider } from "./context/NotesContext.jsx";
+import { SearchProvider } from "./context/SearchContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </AuthProvider>
+    <Router>
+      <AuthProvider>
+        <SocketProvider>
+          <ThemeProvider>
+            <SearchProvider>
+              <LabelsProvider>
+                <NotificationProvider>
+                  <NotesProvider>
+                    <App />
+                  </NotesProvider>
+                </NotificationProvider>
+              </LabelsProvider>
+            </SearchProvider>
+          </ThemeProvider>
+        </SocketProvider>
+      </AuthProvider>
+    </Router>
   </StrictMode>,
 )
